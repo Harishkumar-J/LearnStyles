017b82d79606e81000807b581cffedd503bec15b
Harishkumar J <harishwells@gmail.com>
2022-08-25 16:03:38 +0530

    Conditional styling-Dynamic Inline

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+<<<<<<< HEAD
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
@@ -24,6 +25,45 @@ const CourseInput = props => {
       <Button type="submit">Add Goal</Button>
     </form>
   );
+=======
+const CourseInput = (props) => {
+	const [enteredValue, setEnteredValue] = useState('');
+	const [validGoal, setValidGoal] = useState(true);
+	const goalInputChangeHandler = (event) => {
+		if (event.target.value.trim().length > 0) {
+			setValidGoal(true);
+		}
+		setEnteredValue(event.target.value);
+	};
+
+	const formSubmitHandler = (event) => {
+		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			setValidGoal(false);
+			return;
+		}
+		props.onAddGoal(enteredValue);
+	};
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div className='form-control'>
+				<label style={{ color: validGoal ? 'black' : 'red' }}>
+					Course Goal
+				</label>
+				<input
+					style={{
+						borderColor: validGoal ? '#ccc' : 'red',
+						background: validGoal ? 'transparent' : 'salmon',
+					}}
+					type='text'
+					onChange={goalInputChangeHandler}
+				/>
+			</div>
+			<Button type='submit'>Add Goal</Button>
+		</form>
+	);
+>>>>>>> f4ab9a8 (Start Over)
 };
 
 export default CourseInput;
