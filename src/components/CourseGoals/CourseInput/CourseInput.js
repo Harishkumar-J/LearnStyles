19db6c10d92ecde17f591037efa354d0fa277157
Harishkumar J <harishwells@gmail.com>
2022-08-25 16:15:14 +0530

    ...

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,29 +3,6 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-<<<<<<< HEAD
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
-
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
-
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
-=======
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState('');
 	const [validGoal, setValidGoal] = useState(true);
@@ -63,7 +40,6 @@ const CourseInput = (props) => {
 			<Button type='submit'>Add Goal</Button>
 		</form>
 	);
->>>>>>> f4ab9a8 (Start Over)
 };
 
 export default CourseInput;
